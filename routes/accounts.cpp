be8bd7070f9412a757ad883fc471a6f6e5b6b75c
theluqmn <luqman.yazrie@outlook.com>
2025-01-12 16:32:54 +0800

    opening accounts work and is implemented to the api

--- routes/accounts.cpp
+++ routes/accounts.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include "routes.h"
 
+#include "../functions/accounts/accounts.h"
+
 using namespace std;
 
 void setupAccountsRoutes(crow::SimpleApp& app) {
     CROW_ROUTE(app, "/accounts").methods(crow::HTTPMethod::GET)([](){
         std::cout << "GET '/accounts' route called" << endl;
         return crow::response(200, "Accounts.");
+    });
+
+    CROW_ROUTE(app, "/accounts").methods(crow::HTTPMethod::POST)([](){
+        std::cout << "POST '/accounts' route called" << endl;
+        int open = openAccount("savings", "Luqman", "password");
+        if (open == -1) {
+            return crow::response(500, "Error opening account.");
+        }
+        return crow::response(200, "Account opened with ID: " + to_string(open));
     });
 };
