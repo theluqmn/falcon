203c3c0318473abdfe6df3d6186f56bf562e4da4
theluqmn <luqman.yazrie@outlook.com>
2025-01-12 15:20:58 +0800

    my head hurts

--- functions/accounts.cpp
+++ functions/accounts.cpp
@@ -5,12 +5,9 @@ using namespace std;
 
 sqlite3* initAccountsDB() {
     sqlite3* db;
-    int exit = sqlite3_open("./database/accounts.db", &db);
-    if (exit != SQLITE_OK) {
+    int accounts_db = sqlite3_open("./database/accounts.db", &db);
+    if (accounts_db != SQLITE_OK) {
         cout << "Error opening database" << endl;
         return nullptr;
     }
-
-    // create tables (if it doesn't exist)
-    
 }
