89fad1e2d7621f5f27bcf2af2ece77eb3be65343
theluqmn <luqman.yazrie@outlook.com>
2025-01-15 03:19:33 +0800

    get account balance

--- functions/accounts/utilities.cpp
+++ functions/accounts/utilities.cpp
@@ -36,6 +36,37 @@ string getPassword(string accountType, int accountID) {
     return password;
 }
 
+float getBalance(string accountType, int accountID) {
+    // open the database
+    sqlite3* db = initAccountsDB();
+    if (db == nullptr) {
+        return -1;
+    }
+
+    // get balance
+    string sql = "SELECT balance FROM " + accountType + " WHERE id = " + to_string(accountID);
+    sqlite3_stmt* stmt;
+    int res = sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, NULL);
+    if (res != SQLITE_OK) {
+        cerr << "Error preparing statement: " << sqlite3_errmsg(db) << endl;
+        sqlite3_close(db);
+        return -1;
+    }
+
+    res = sqlite3_step(stmt);
+    if (res != SQLITE_ROW) {
+        cerr << "Error getting balance: " << sqlite3_errmsg(db) << endl;
+        sqlite3_finalize(stmt);
+        sqlite3_close(db);
+        return -1;
+    }
+
+    float balance = (float)sqlite3_column_double(stmt, 0);
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
+    return balance;
+}
+
 int accountExists(string accountType, int accountID) {
     // open the database
     sqlite3* db = initAccountsDB();
