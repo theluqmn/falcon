a95cc36c02d95f7b2d36b8dca9ea70a158d5f02c
theluqmn <luqman.yazrie@outlook.com>
2025-01-15 18:08:05 +0800

    starting work on logic for transactions

---
+++ functions/transactions/new.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <sqlite3.h>
+#include <string>
+
+#include "../accounts/accounts.h"
+
+using namespace std;
+
+int newTransaction(string senderType, int senderID, string receiverType, int receiverID, float amount, string description) {
+    
+}
